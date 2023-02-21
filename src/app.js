95e60b9922a9b4ea70da3701cb963b8a0c8d690e
lthurler <leothurler@gmail.com>
2023-02-21 19:54:35 -0300

    Fim da aula 6

--- src/app.js
+++ src/app.js
@@ -7,14 +7,24 @@ app.use(express.json())
 
 //mock
 const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    { id: 1, selecao: 'Brasil', grupo: 'G' },
+    { id: 2, selecao: 'Suíça', grupo: 'G' },
+    { id: 3, selecao: 'Camarões', grupo: 'G' },
+    { id: 4, selecao: 'Sérvia', grupo: 'G' }
 ]
 
+//retornar o objeto por id
+function buscarSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+//pegar a posição ou index do elemento no array por id
+function buscarIndexSelecao(id) {
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+
 //cria rota padrão ou raiz
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('curso NodeJS')
 })
 
@@ -22,9 +32,26 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
-app.post('/selecoes',(req,res) => {
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
+app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('seleção cadastrada com sucesso')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com id ${req.params.id} excluida com sucesso!`)
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
 })
 
 export default app
